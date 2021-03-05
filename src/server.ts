548c74dde490ce9aa9063dbf4701b46dd98c8764
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2021-03-05 01:26:11 -0500

    upload

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import routes from './router';
 import mongoose from 'mongoose'
 const port = process.env.PORT || 3000;
 const app = express();
-
-
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect('mongodb://mongoso:1nf0rm4t1c4@geonosis.mongodb.umbler.com:48555/mongoso', {
     useCreateIndex: true,
@@ -16,8 +16,16 @@ mongoose.connect('mongodb://mongoso:1nf0rm4t1c4@geonosis.mongodb.umbler.com:4855
     console.log('connected')
 })
 
-app.use(cors());
-app.use(express.json());
+
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(routes)
 
 
